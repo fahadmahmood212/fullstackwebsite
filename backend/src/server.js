77da3e534bdcbb07409386d8f46807498e8d7e37
fahadmahmood212 <mahmood01fah@gmail.com>
2021-01-28 18:04:54 +0000

    prepared for deloying

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // parses the json object included in the request body
 app.use(bodyParser.json()); 
